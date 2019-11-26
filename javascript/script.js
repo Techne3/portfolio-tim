02585f14179ed73e31bfd46866ba5fe64923a7d1
Timothy Sharkey <Techne3@gmail.com>
2019-11-26 13:31:47 -0700

    Added layout for portfolio section Added  isotope and fancyBox library for port animations Set navigation to sticky once offset is reached added loading spinner animation

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,27 @@
+// setting the time for the loader to fade out
+$(window).on("load", function(){
+
+  $(".loader").fadeOut(500, function() {
+    $(".loader").fadeOut(750)
+  });
+
+    //Isotope 
+    $('.items').isotope({
+      filter:'*',
+      animationOptions:{
+          duration:1500,
+          easing:'linear',
+          queue:false
+      }
+    })
+})
+
+
+
+
 $(document).ready(function() {
+
+  //superslides 
   $(`#slides`).superslides({
     animation: "fade",
     play: 4000
@@ -54,4 +77,58 @@ $(document).ready(function() {
       });
     }
   });
+  
+  //FancyBox plugin
+  $("[data-fancybox]").fancybox()
+
+
+
+  // filter with isotope
+  $("#filters a").click(function(){
+
+    $("#filters .current").removeClass("current");
+    $(this).addClass("current");
+
+    let selector = $(this).attr("data-filter");
+
+    $('.items').isotope({
+      filter: selector,
+      animationOptions:{
+          duration:1500,
+          easing:'linear',
+          queue:false
+      }
+    })
+      return false;
+  })
+
+
+// make navigation sticky once it gets past hero section
+  const nav = $("#navigation");
+  const navTop = nav.offset().top;
+
+  $(window).on("scroll", stickyNavigation)
+
+  function stickyNavigation() {
+    let body =$("body");
+    if($(window).scrollTop() >= navTop){
+      // fix the jumping once fixed nav is applied 
+      body.css("padding-top", nav.outerHeight() + "px")
+      body.addClass("fixedNav");
+    }else{
+      body.css("padding-top", 0)
+      body.removeClass("fixedNav")
+    }
+  }
+
+
+//Smooth scroll
+$("#navigation li a").click(function(e){
+    e.preventDefault();
+
+    let targetElement = $(this).attr("href");
+    let targetPosition = $(targetElement).offset().top;
+    $("html, body").animate({scrollTop: targetPosition - 50}, "slow");
+});
+
 });
